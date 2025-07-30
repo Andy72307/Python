fa74cb9984a067ed0ef9047ad07cbfd946b36113
Andres <aglagosb07@gmail.com>
2025-07-30 14:17:09 -0500

    Funcion saludar

---
+++ funciones.py
@@ -0,0 +1,9 @@
+print("*** Funciones en python ***")
+
+#Definir una funcion para saludar 
+
+def saludar():
+    print("Saludos desde una funcion")
+
+#Llamar a la funcion en el programa principal 
+saludar()
