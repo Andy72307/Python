633fd19df5df93d0469386d89164ba9b26eecab7
Andres <aglagosb07@gmail.com>
2025-07-30 14:40:31 -0500

    Primera forma de importar un modulo

--- funcionSumar.py
+++ funcionSumar.py
@@ -1,11 +1,7 @@
+import moduloFuncionSumar
+
 print("*** Funcion Sumar ***")
 
-#Definir funcion sumar
-
-def sumar(a, b):
-    res = a + b
-    print(f'La suma de {a} + {b} es igual a: {res}')
-
 #Llamar a la funcion
-sumar(54, 100)
-sumar(7, 8)
+moduloFuncionSumar.sumar(54, 100)
+moduloFuncionSumar.sumar(7, 8)
