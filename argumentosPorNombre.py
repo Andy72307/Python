594bb28c95f005031f6cd360bfdddf561f600daf
Andres <aglagosb07@gmail.com>
2025-07-30 22:38:14 -0500

    Argumentos con valor por default

--- argumentosPorNombre.py
+++ argumentosPorNombre.py
@@ -1,6 +1,6 @@
 print("*** Funcion con argumentos por nombre ***")
 
-def imprimirPersona(nombre, apellido, edad):
+def imprimirPersona(nombre, apellido = '', edad = 0):
     print(f'La persona: {nombre} {apellido} tiene {edad} años')
 
 # Llamar la funcion pasando los argumentos de manera posicional
@@ -11,3 +11,8 @@ imprimirPersona(nombre = 'Juan', apellido = "Leon", edad = 19)
 
 # Llamar la funcion usando argumentos por nombre, pero intercambiando el orden
 imprimirPersona(edad = 48, nombre = "Sandra", apellido = 'Ballesteros')
+
+# Argumentos con valor por default
+imprimirPersona(nombre = 'Miguel')
+imprimirPersona(nombre = 'Miguel', apellido = 'Lagos')
+imprimirPersona(apellido = 'Lagos', nombre = 'Miguel')
