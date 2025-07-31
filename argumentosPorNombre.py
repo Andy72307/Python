7c9cc78cf3707f63369175ffaf951e1d90584f57
Andres <aglagosb07@gmail.com>
2025-07-30 22:12:58 -0500

    Llamar la funcion pasando los argumentos de manera posicional

---
+++ argumentosPorNombre.py
@@ -0,0 +1,7 @@
+print("*** Funcion con argumentos por nombre ***")
+
+def imprimirPersona(nombre, apellido, edad):
+    print(f'La persona: {nombre} {apellido} tiene {edad} años')
+
+# Llamar la funcion pasando los argumentos de manera posicional
+imprimirPersona('Andres', 'Lagos', 18)
