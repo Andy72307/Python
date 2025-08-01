dba91eaa2ffb87a1fdcc79f6214b25407e3a6e85
Andres <aglagosb07@gmail.com>
2025-07-31 19:44:33 -0500

    Caracteres especiales en python (\n, \t, \\)

---
+++ caracteres_especiales.py
@@ -0,0 +1,6 @@
+# Caracteres especiales
+print('Hola \nMundo') #\n salto de linea
+print('\t\tPython \t\tes genial')
+print('Juan\'"Perez')
+print("Karla\"Gomez")
+print('Caracter \\ diagonal invertida')
