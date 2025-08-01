b71d04f4b9370e1459cebcdb5e982e8a954e9570
Andres <aglagosb07@gmail.com>
2025-07-31 19:40:53 -0500

    Sistema basico para un hotel

---
+++ Sistema Hoteles.py
@@ -0,0 +1,27 @@
+print('*** Sistema de reserva de hoteles ***')
+
+# Definimos las variables
+nombre_cliente = 'Laura Martinez'
+Dias_estancia = 5
+Tarifa_diaria = 1200.0
+Vista_mar = True
+
+# Mostrar el detalle de la reserva
+print ('Cliente:', nombre_cliente)
+print ('Días de estancia:', Dias_estancia)
+print ('Tarifa diaria:', Tarifa_diaria)
+print ('¿Habitación con vista al mar?', Vista_mar )
+
+# Realizamos algunas modificaciones
+Dias_estancia = 7
+Tarifa_diaria = 1000.00
+Vista_mar = False
+
+# Mostrar el detalle de la reserva
+print ()
+print ('Cliente:', nombre_cliente)
+print ('Días de estancia:', Dias_estancia)
+print ('Tarifa diaria:', Tarifa_diaria)
+print ('¿Habitación con vista al mar?', Vista_mar )
+
+
