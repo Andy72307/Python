00ba1fd23551cbb948114d2cfe656778dfe288f8
Andres <aglagosb07@gmail.com>
2025-07-31 19:41:18 -0500

    Sistema basico para una tienda online

---
+++ Tienda_Online.py
@@ -0,0 +1,26 @@
+print('*** Sistema de Tienda Online ***')
+# Definir variables
+Nombre_Producto = 'Cámara digital'
+Precio_Producto = 399.99
+Cantidad_Producto = 20
+disponibilidad = True
+
+# Imprimir en pantalla
+
+print ('Producto:', Nombre_Producto)
+print ('Precio:', Precio_Producto, '$')
+print ('Cantidad inventario:', Cantidad_Producto)
+print ('Disponible para entrega:', disponibilidad)
+
+# Ajustes a variables
+
+Precio_Producto = 299.99
+Cantidad_Producto = 10
+disponibilidad = True
+
+# Imprimir en pantalla
+print ()
+print ('Producto:', Nombre_Producto)
+print ('Precio:', Precio_Producto, '$')
+print ('Cantidad inventario:', Cantidad_Producto)
+print ('Disponible para entrega:', disponibilidad)
