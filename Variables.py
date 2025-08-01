8b0f1ab42ab2735b97eb67a92155b6acf42c1b6b
Andres <aglagosb07@gmail.com>
2025-07-31 19:37:55 -0500

    Declaracion e inicializacion de variables

---
+++ Variables.py
@@ -0,0 +1,32 @@
+# Variables en Python
+
+# Declaracion e inicializacion de variables
+
+edad = 17
+altura = 1.66
+pais = 'Colombia'
+
+# Acceder a las variables
+print("Valores inicianles: ")
+print('Edad:', edad)
+print('Altura:', altura)
+print('Pais:', pais)
+
+# Modificar el valor de una variable
+edad = 30
+altura = 1.71
+
+# Acceder a las variables
+print("Valores modificados: ")
+print('Edad:', edad)
+print('Altura:', altura)
+print('Pais:', pais)
+
+#En python el tipo es dinamico
+
+edad = 'Treinta'
+print("Edad:",edad)
+
+#Si queremos acceder a una variable no declarada manda error
+telefono = 3147090649
+print("Telefono:", telefono)
