c00b56ac49c92494f640eb32fa9d98a615c212b4
Andres <aglagosb07@gmail.com>
2025-07-31 19:54:54 -0500

    Reemplzar subcadenas

---
+++ subcadena_replace.py
@@ -0,0 +1,6 @@
+# Remplazar subcadenas
+
+cadena = 'Hola Mundo'
+print(f'Cadena original: {cadena}')
+print(f'Subcadena remplazada: {cadena.replace('Mundo','Andrés')}')
+print(f'Subcadena con adios: {cadena.replace('Hola', 'Adios')}')
