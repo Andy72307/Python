46073d1ec4b652c79133b2c059fd5af05613e411
Andres <aglagosb07@gmail.com>
2025-08-06 01:26:20 -0500

    Carpeta Variables

---
+++ Variables/Tipos_Datos.py
@@ -0,0 +1,21 @@
+# Ejemplo de tipos de datos en Python
+
+# Entero
+edad = 17
+print('Edad:', edad)
+
+# Numero con punto flotante
+altura = 1.66
+print('Altura:', altura)
+
+# Cadena de texto
+nombre = 'Andrés'
+print('Nombre:', nombre)
+
+# Tipo boolean
+es_estudiante = False
+print('Es estudiante?', es_estudiante)
+
+# None, ausencia de valor
+direccion = None
+print('Direccion:',direccion)
